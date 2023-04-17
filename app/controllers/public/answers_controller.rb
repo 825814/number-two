64a8e916bcc95f48938f9be7a6ccc8925ecfb455
825814 <120297399+825814@users.noreply.github.com>
2023-04-17 12:06:49 +0000

    [add]回答入力画面

--- app/controllers/public/answers_controller.rb
+++ app/controllers/public/answers_controller.rb
@@ -3,6 +3,33 @@ class Public::AnswersController < ApplicationController
 
   def new
     @question = Question.find(params[:question_id])
-    # @answer = Answer.new
+    @answer = Answer.new
   end
+
+  def create
+    @answer = Answer.new(answer_params)
+
+    @answer.user_id = current_user.id
+    @answer.question_id = params[:question_id]
+    @answer.choice_id = params[:choice_id]
+    if @answer.save
+      flash[:notice] = "成功！"
+      redirect_to("/questions/complete")
+    else
+      flash.now[:alert] = "失敗！"
+      render "new"
+    end
+  end
+
+  def confirm
+    @answers = current_user.answers
+    @answer = Answer.new(answer_params)
+    @question = Question.find(params[:question_id])
+  end
+
+private
+  def answer_params
+    params.require(:answer).permit(:comment, :choice_id)
+  end
+
 end
