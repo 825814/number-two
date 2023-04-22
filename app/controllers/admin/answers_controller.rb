2db79a97eca3e64c9990f8628c31b95866e96def
825814 <120297399+825814@users.noreply.github.com>
2023-04-22 02:33:09 +0000

    [update]ランキング機能途中

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -2,12 +2,9 @@ class Admin::AnswersController < ApplicationController
 
   def index
     @question = Question.find(params[:question_id])
-    # @choice = Choice.find(params[:choice_id])
-    @choices = Choice.all
     @answers = @question.answers
 
-    # @all_ranks = Answer.find(Choice.group(:answer_id).order('count(answer_id) desc').pluck(:answer_id))
-     @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
+    @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
     # @all_ranks = Note.find(Like.group(:note_id).order('count(note_id) desc').limit(3).pluck(:note_id))
   end
 
