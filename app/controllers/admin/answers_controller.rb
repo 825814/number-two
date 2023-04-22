6e02fe6ea9e47644a1e1638d44a998cf41cecf26
825814 <120297399+825814@users.noreply.github.com>
2023-04-22 12:19:04 +0000

    [update]Admin回答表示

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -2,8 +2,8 @@ class Admin::AnswersController < ApplicationController
 
   def index
     @question = Question.find(params[:question_id])
+    # @answers = @question.answers
     @answers = @question.answers
-
     @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
     # @all_ranks = Note.find(Like.group(:note_id).order('count(note_id) desc').limit(3).pluck(:note_id))
   end
@@ -11,7 +11,14 @@ class Admin::AnswersController < ApplicationController
   def show
     @question = Question.find(params[:question_id])
     @choice = Choice.find(params[:choice_id])
-    # ?他のanswerも全部表示される　Answer.allだと
-    @answers = @choice.answers
+    
+    
+    # ?他のanswerも全部表示されるAnswer.allだと
+    # @choices = @question.choices
+    # @answers = @choice.answers
+    @answers = @question.answers
+    # && @answers =  @choice.answers
+
+
   end
 end
