5e372360ca67a455c295355345b5712d993a2e1f
825814 <120297399+825814@users.noreply.github.com>
2023-04-23 07:10:32 +0000

    [update]質問と選択肢に関連した回答が表示できた。

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -2,8 +2,10 @@ class Admin::AnswersController < ApplicationController
 
   def index
     @question = Question.find(params[:question_id])
-    # @answers = @question.answers
+
+    @questions = Question.all
     @answers = @question.answers
+
     @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
     # @all_ranks = Note.find(Like.group(:note_id).order('count(note_id) desc').limit(3).pluck(:note_id))
   end
@@ -15,10 +17,11 @@ class Admin::AnswersController < ApplicationController
 
     # ?他のanswerも全部表示されるAnswer.allだと
 
-    @answers = @choice.answers
-    @answers = @question.answers
+    # @answers = @choice.answers
+    # @answers = @question.answers
+    @answers = @question.answers & @choice.answers
     # この２つをまとめる
-    
+
 # Student.where(gender: "女性").where("age > ?", 16)
     # && @answers =  @choice.answers
 
