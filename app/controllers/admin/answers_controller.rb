986de052ce84d998e40d184a3d8234fa27e1a4ef
825814 <120297399+825814@users.noreply.github.com>
2023-04-25 03:17:14 +0000

    [update]ランキング表示ができた

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -1,20 +1,28 @@
 class Admin::AnswersController < ApplicationController
 
-  def index
-    @question = Question.find(params[:question_id])
-    @ranking = Answer.ranking(params[:question_id])
-  end
-
   # def index
   #   @question = Question.find(params[:question_id])
-  #   @questions = Question.all
-  #   @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
-
-  #   @ranks = @question.choices.joins(:answers).group(:choice_id).order('count(choice_id) desc')
-
-  #   @choices = Choice.all
-  #   @answers = Answer.all
+  #   @ranking = Answer.ranking(params[:question_id])
   # end
+
+  def index
+    @question = Question.find(params[:question_id])
+    @questions = Question.all
+    # @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
+    # 全ての質問に対するランキングが表示される（特別賞）
+
+    # @ranks = @question.choices.joins(:answers).group(:choice_id).order('count(choice_id) desc')
+    # @choices = Choice.all
+
+    @answers = @question.answers.includes(:choice)
+    #質問に紐付いた回答を取得する
+    choice_ids = @answers.group(:choice_id).order('count_choice_id DESC').count(:choice_id).keys
+    # choice_ids = @answers.group(:choice_id).order('count_choice_id DESC').limit(5).count(:choice_id).keys
+    @all_ranks = Choice.where(id: choice_ids)
+    #回答から選択肢のIDを集計し、ランキングを作成する
+
+
+  end
 
   def show
     @question = Question.find(params[:question_id])
