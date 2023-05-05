247f666d22a81061e5ceab738ae048e63e6d451a
825814 <120297399+825814@users.noreply.github.com>
2023-05-05 02:03:38 +0000

    [update]エラーメッセージ

--- app/controllers/public/answers_controller.rb
+++ app/controllers/public/answers_controller.rb
@@ -26,7 +26,7 @@ class Public::AnswersController < ApplicationController
       render :new
     end
   end
-# 　選択肢が選択されていなかったり、理由が入力されていなかった場合にエラーメッセージが表示される
+# 選択肢が選択されていなかったり、理由が入力されていなかった場合にエラーメッセージが表示される
 
   # def create
   #   @question = Question.find(params[:question_id])
@@ -56,37 +56,20 @@ class Public::AnswersController < ApplicationController
   #   @question = Question.find(@answer.question_id)
   # end
 
-  # def destroy
-  #   @answer = Answer.new(answer_params)
-  #   @answer.user_id = current_user.id
-  #   @answer.question_id = params[:answer][:question_id]
-  #   @answer.choice_id = params[:answer][:choice_id]
-
-  #   @answer.destroy
-  #     redirect_to questions_path
-  #     flash[:notice] = "すでに回答済みのため、この回答は無効です。"
-  # end
-
   def update
-    # @answer = @question.current_user.answers
+    @question = Question.find(params[:question_id])
     @answer = current_user.answers.find_by(question_id: params[:question_id])
 
-    # @answer.user_id = current_user.id
-    # @answer.question_id = params[:answer][:question_id]
-    # @answer.choice_id = params[:answer][:choice_id]
-
-    if @answer.update!(answer_params)
+    if @answer.update(answer_params)
       redirect_to users_my_page_path
-       flash[:notice] = "回答を変更しました。"
+      flash[:notice] = "回答を変更しました。"
     else
-       flash.now[:alert] = "失敗！"
-      render "new"
+      render "edit"
     end
   end
 
   def edit
     @question = Question.find(params[:question_id])
-    # @answer = Answers.find(params[:id])
     @answer = current_user.answers.find_by(question_id: params[:question_id])
   end
 
