1b1c59dfffaaa86ed1fddf6e90eade1af84af2ef
825814 <120297399+825814@users.noreply.github.com>
2023-05-08 14:47:48 +0000

    [update]articles モデルとコントローラーを削除

--- app/controllers/articles_controller.rb
+++
@@ -1,5 +0,0 @@
-# class ArticlesController < ApplicationController
-#   def search
-#     @articles = Article.search(params[:search])
-#   end
-# end
--- app/models/article.rb
+++
@@ -1,3 +0,0 @@
-# class Article < ApplicationRecord
-#   scope :search, ->(query) { where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%") }
-# end
