d15bca15893eb84ee7c58a40381e3c40a519a62d
825814 <120297399+825814@users.noreply.github.com>
2023-05-09 09:02:54 +0000

    [add]投稿されたテーマがない場合の条件分岐

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,15 +1,21 @@
 class Public::HomesController < ApplicationController
   include ActionView::Helpers::AssetUrlHelper
 
+# 投稿された画像がない場合はno_image.jpgを表示する
+# タイトルがない場合@image_title = "Untitled"ビューも@image.titleから@image_titleに変更
   def top
     @image = Image.first
     if @image.blank?
       @image_url = asset_path('no_image.jpg')
+    elsif @image.title.blank?
+      @image_url = url_for(@image.image)
+      @image_title = "Untitled"
     else
       @image_url = url_for(@image.image)
+      @image_title = @image.title
     end
   end
-# 投稿された画像がない場合はno_image.jpgを表示する
+
 
 end
 
