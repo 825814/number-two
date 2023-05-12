0ad1e84ea92af4f96020c1e086eee2569f978bdb
825814 <120297399+825814@users.noreply.github.com>
2023-05-12 11:05:58 +0000

    [update]ランキング機能修正

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -3,41 +3,35 @@ before_action :authenticate_admin!
 
   def index
     @question = Question.find(params[:question_id])
-    @questions = Question.all
     # @all_ranks = Choice.find(Answer.group(:choice_id).order('count(choice_id) desc').pluck(:choice_id))
     # 全ての質問に対するランキングが表示される（特別賞）
-
-    # @ranks = @question.choices.joins(:answers).group(:choice_id).order('count(choice_id) desc')
-    # @choices = Choice.all
-
     @answers = @question.answers.includes(:choice)
     #質問に紐付いた回答を取得する
-    choice_ids = @answers.group(:choice_id).order('count_choice_id DESC').count(:choice_id).keys
-    # choice_ids = @answers.group(:choice_id).order('count_choice_id DESC').limit(5).count(:choice_id).keys
-    @all_ranks = Choice.where(id: choice_ids)
+    # choice_ids = @answers.group(:choice_id).order('count_choice_id DESC').count(:choice_id).keys
+    # @all_ranks = Choice.where(id: choice_ids)
+    # 失敗。このとき、count_choice_id カラムは存在しないためランキングができない
+
     #回答から選択肢のIDを集計し、ランキングを作成する
-
-
+    choice_ids = @answers.group(:choice_id).order('count(choice_id) DESC').count.keys
+    @all_ranks = Choice.where(id: choice_ids).sort_by { |choice| choice_ids.index(choice.id) }
+    # count(choice_id) を使用して選択肢のIDを集計し、正しい順序で並び替える。
+    # Choice.where で取得した選択肢を
+    # sort_by メソッドを使用して
+    # choice_ids の順序に並び替えている。
   end
+
 
   def show
     @question = Question.find(params[:question_id])
-
     @choice = Choice.find(params[:choice_id])
-
-
     # ?他のanswerも全部表示されるAnswer.allだと
-
     # @answers = @choice.answers
     # @answers = @question.answers
     @answers = @question.answers & @choice.answers
     # この２つをまとめる
-
-
     @favorite_comments = current_admin.favorites.map{ |f| f.answer.comment }.compact
     @comments = @answers.map(&:comment) & @favorite_comments
   end
 
-  #
 
 end
