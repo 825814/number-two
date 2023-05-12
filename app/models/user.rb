54235ea473ae12a33cdb7f7be32fbb997bc3c857
825814 <120297399+825814@users.noreply.github.com>
2023-05-12 11:51:29 +0000

    [update]新規登録で名前が必須である

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
   # has_one_attached :image
   # include Devise::Models::Guest
   # Include default devise modules. Others available are:
